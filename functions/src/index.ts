a719c37c4a19eafcaac0941f32e082c6d8536c8e
tetsu4819 <tetsu4819@gmail.com>
2021-08-02 01:37:47 +0900

    :hammer: add LINEbot

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,67 @@
 import * as functions from "firebase-functions";
+import {
+  Client,
+  SignatureValidationFailed,
+  TextMessage,
+  validateSignature,
+  WebhookEvent,
+} from "@line/bot-sdk";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db: FirebaseFirestore.Firestore = admin.firestore();
+
+const client: Client = new Client({
+  channelAccessToken: functions.config().line.channel_access_token,
+  channelSecret: functions.config().line.channel_secret,
+});
+
+export const lineWebhook = functions
+  .region("asia-northeast1")
+  .https.onRequest(async (request, response) => {
+    const signature = request.get("x-line-signature");
+    const channelSecret = functions.config().line.channel_secret;
+
+    if (
+      !signature ||
+      !validateSignature(request.rawBody, channelSecret, signature)
+    ) {
+      throw new SignatureValidationFailed(
+        "signature validation failed",
+        signature
+      );
+    }
+
+    Promise.all(request.body.events.map(lineEventHandler))
+      .then((result) => response.json(result))
+      .catch((error) => console.error(error));
+  });
+
+const lineEventHandler = (event: WebhookEvent) => {
+  if (event.type !== "message") {
+    console.log("event type is not message");
+    return Promise.resolve(null);
+  }
+  try {
+    if (event.message.type === "text") {
+      const message: TextMessage = {
+        type: "text",
+        text: event.message.text,
+      };
+      //write to firestore
+      db.collection("messages").add(message);
+
+      // reply echo message
+      const reply: TextMessage = {
+        type: "text",
+        text: "send message!",
+      };
+      return client.replyMessage(event.replyToken, reply);
+    } else {
+      return Promise.resolve(null);
+    }
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    return Promise.resolve(null);
+  }
+};
